e7aff34a4bd94c466c7e796ea55dbf3e3942bdff
patrickstecker <Stecker2000@gmail.com>
2020-10-16 13:42:09 +0200

    Added close Button to Lecture View and view is now starting at current day

--- app/src/main/java/com/patrickstecker/alarmnotificator/activities/LecturePlanActivity.kt
+++ app/src/main/java/com/patrickstecker/alarmnotificator/activities/LecturePlanActivity.kt
@@ -1,11 +1,13 @@
 package com.patrickstecker.alarmnotificator.activities
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.patrickstecker.alarmnotificator.R
 import com.patrickstecker.alarmnotificator.Storage
 import me.relex.circleindicator.CircleIndicator3
+import java.util.*
 
 class LecturePlanActivity: AppCompatActivity() {
 
@@ -14,6 +16,7 @@ class LecturePlanActivity: AppCompatActivity() {
         setContentView(R.layout.activity_lecture_plan)
         val week = Storage.getLectures()
         val viewPager2 = findViewById<ViewPager2>(R.id.view_pager_2)
+        val btnClose = findViewById<ImageButton>(R.id.close)
 
         viewPager2.adapter =
             ViewPagerAdapter(
@@ -24,5 +27,17 @@ class LecturePlanActivity: AppCompatActivity() {
 
         val indicator = findViewById<CircleIndicator3>(R.id.indicator)
         indicator.setViewPager(viewPager2)
+
+        val cal = Calendar.getInstance()
+        var start = (cal.get(Calendar.DAY_OF_WEEK) -2)
+        if (start < 0)
+            start += 7
+        viewPager2.post {
+            viewPager2.setCurrentItem(start, false)
+        }
+
+        btnClose.setOnClickListener {
+            finish()
+        }
     }
 }
